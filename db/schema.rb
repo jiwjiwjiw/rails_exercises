9f4b296ab640fe35927ef3c28cf76fc7b423e221
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-24 20:38:10 +0200

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180824145807) do
+ActiveRecord::Schema.define(version: 20180824183701) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "done_count"
   end
 
 end
