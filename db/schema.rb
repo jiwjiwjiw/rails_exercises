6ba80c22102ca6480b0cb51d558ee202dcc39bb6
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-27 08:35:45 +0200

    generated migration for photo_url and migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180824183701) do
+ActiveRecord::Schema.define(version: 20180827063500) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "done_count"
+    t.string "photo_url"
   end
 
 end
