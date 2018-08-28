22638f1c9841a3078ed651a30fe1386dcecda8bc
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-28 11:15:11 +0200

    added test code

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -10,6 +10,20 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
    end
 
    test 'style guide navigation' do
-     
+     visit('/')
+     click_on('Styleguide', match: :first)
+     sleep(3.seconds)
+     assert current_url == styles_index_path
+     assert has_content?('Atoms')
+     assert has_content?('Molecules')
+     click_on('Molecules', match: :first)
+     sleep(3.seconds)
+     assert current_url == styles_molecules_path
+     assert has_content?('Card')
+     click_on('My Bucket List', match: :first)
+     sleep(3.seconds)
+     assert current_url == root_path
+     click_on('My Ideas', match: :first)
+     sleep(5.seconds)
    end
  end
