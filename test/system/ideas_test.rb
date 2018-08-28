1d2e75de6e6b619aac37f759021888a8955d8eec
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-28 13:34:32 +0200

    added test that checks if two ideas added to the database are correctly displayed

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -10,4 +10,24 @@ class IdeasTest < ApplicationSystemTestCase
     click_on 'Create an idea'
     assert page.has_content? title
   end
+
+  test 'display ideas in database' do
+    idea1 = Idea.new( title: 'See the northern lights',
+                      done_count: 3,
+                      photo_url: 'http://fpoimg.com/255x170')
+    idea1.save!
+    idea2 = Idea.new( title: 'Swim with dolphins',
+                      done_count: 3,
+                      photo_url: 'http://fpoimg.com/255x170')
+    idea2.save!
+    idea3 = Idea.new( title: 'Shake hands with the president',
+                      done_count: 3,
+                      photo_url: 'http://fpoimg.com/255x170')
+    idea3.save!
+    visit account_ideas_path
+    assert page.has_content? idea1.title
+    assert page.has_content? idea2.title
+    assert page.has_content? idea3.title
+    assert_equal 3, page.all('.card').count
+  end
 end
