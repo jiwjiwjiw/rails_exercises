306ed0c7b0864fd98bc6b5d0a28b06d78236e498
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-31 12:18:29 +0200

    added empty idea_params method

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -27,4 +27,11 @@ class IdeasController < ApplicationController
     idea.update(params)
     redirect_to(account_ideas_path)
   end
+
+  private
+
+  def idea_params
+    # params.permit(:title, :description, :photo_url, :done_count, :name_of_user)
+  end
+
 end
