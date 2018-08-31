18faa22266d5d4f1afed25a82b3f331f4438d8bf
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-08-31 19:07:40 +0200

    added test for comment reassignment to other idea

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'changing the associated Idea for a Comment' do
+    idea_1 = Idea.new title: 'some title'
+    idea_1.save!
+    comment = Comment.new body: "I'd like to do this!", idea: idea_1
+    comment.save!
+    idea_2 = Idea.new title: 'some other title'
+    idea_2.save!
+    comment.idea = idea_2
+    comment.save!
+    assert_equal idea_2 Comment.first.idea
+  end
 end
