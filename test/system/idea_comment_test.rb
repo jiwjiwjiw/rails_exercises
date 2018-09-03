17b62d0616263801caf7b4f713ded310d61ce25c
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-09-03 22:48:23 +0200

    added test for new comment creation

---
+++ test/system/idea_comment_test.rb
@@ -0,0 +1,12 @@
+require "application_system_test_case"
+
+class IdeaCommentsTest < ApplicationSystemTestCase
+  test 'adding a Comment to an Idea' do
+    idea = Idea.new title: 'Test idea'
+    idea.save!
+    visit idea_path(idea)
+    fill_in 'Body', with: 'Some dummy comment'
+    click_on 'Create Comment', match: :first
+    assert_equal idea_path(idea), current_path
+  end
+end
