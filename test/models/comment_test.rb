93e77fac247da6c48c1c1f96db6b05ea859fd27c
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-09-04 08:42:29 +0200

    fixed tests after associating comment to user

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,7 +4,8 @@ class CommentTest < ActiveSupport::TestCase
   test 'changing the associated Idea for a Comment' do
     idea_1 = Idea.new title: 'some title'
     idea_1.save!
-    comment = Comment.new body: "I'd like to do this!", idea: idea_1
+    user = User.new
+    comment = Comment.new body: "I'd like to do this!", idea: idea_1, user: user
     comment.save!
     idea_2 = Idea.new title: 'some other title'
     idea_2.save!
@@ -16,7 +17,8 @@ class CommentTest < ActiveSupport::TestCase
   test 'cascading save' do
     idea_1 = Idea.new title: 'some title'
     idea_1.save!
-    comment = Comment.new body: "Great idea!"
+    user = User.new
+    comment = Comment.new body: "Great idea!", user: user
     idea_1.comments << comment
     idea_1.save!
     assert_equal comment, Comment.first
