2a1796fb0bda6b762f16f60873f37cefed6f3319
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-09-04 09:38:37 +0200

    added user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.find_or_create_by(user_params)
+    user.save!
+    redirect_to ideas_path
+  end
 
+  def user_params
+    params.permit(:email)
   end
 
 end
