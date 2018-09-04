fd09c01de8147204195e230c24833a865b169a13
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-09-04 12:15:49 +0200

    fixed model tests that failed after introducing user-idea relationship

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,11 +3,14 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
   test 'changing the associated Idea for a Comment' do
     idea_1 = Idea.new title: 'some title'
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     user = User.new
     comment = Comment.new body: "I'd like to do this!", idea: idea_1, user: user
     comment.save!
     idea_2 = Idea.new title: 'some other title'
+    idea_2.user = user
     idea_2.save!
     comment.idea = idea_2
     comment.save!
@@ -16,6 +19,8 @@ class CommentTest < ActiveSupport::TestCase
 
   test 'cascading save' do
     idea_1 = Idea.new title: 'some title'
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     user = User.new
     comment = Comment.new body: "Great idea!", user: user
--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -4,9 +4,12 @@ class IdeaTest < ActiveSupport::TestCase
   test 'the partially complete Idea created is first in the list' do
     first_idea = Idea.new
     first_idea.title = 'first idea'
+    user = User.new email: 'jiw@netplus.ch'
+    first_idea.user = user
     first_idea.save!
     second_idea = Idea.new
     second_idea.title = 'second idea'
+    second_idea.user = user
     second_idea.save!
     assert_equal(first_idea, Idea.all.first)
   end
@@ -16,11 +19,14 @@ class IdeaTest < ActiveSupport::TestCase
     first_idea.title = 'Cycle the length of the United Kingdom'
     first_idea.photo_url = 'http://mybucketlist.ch/an_image.jpg'
     first_idea.done_count = 12
+    user = User.new email: 'jiw@netplus.ch'
+    first_idea.user = user
     first_idea.save!
     second_idea = Idea.new
     second_idea.title = 'Visit Japan'
     second_idea.photo_url = 'http://mybucketlist.ch/second_image.jpg'
     second_idea.done_count = 3
+    second_idea.user = user
     second_idea.save!
     assert_equal(first_idea, Idea.all.first)
   end
@@ -28,6 +34,8 @@ class IdeaTest < ActiveSupport::TestCase
   test 'updated_at is changed after updating title' do
     idea = Idea.new
     idea.title = 'Visit Marrakech'
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
     idea.save!
     first_updated_at = idea.updated_at
     idea.title = 'Visit the market in Marrakech'
@@ -39,6 +47,8 @@ class IdeaTest < ActiveSupport::TestCase
     idea = Idea.new
     idea.title = 'some title'
     idea.done_count = 34
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
     idea.save!
     first_updated_at = idea.updated_at
     idea.done_count = 151
@@ -50,6 +60,8 @@ class IdeaTest < ActiveSupport::TestCase
     idea = Idea.new
     idea.title = 'some idea'
     idea.photo_url = 'turtle-big.jpg'
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
     idea.save!
     first_updated_at = idea.updated_at
     idea.photo_url = 'northern-lights.jpg'
@@ -60,6 +72,8 @@ class IdeaTest < ActiveSupport::TestCase
   test 'search with one matching result' do
     idea = Idea.new
     idea.title = 'Stand at the top of the Empire State Building'
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
     idea.save!
     assert_equal(1, Idea.search('the top').length())
   end
@@ -67,6 +81,8 @@ class IdeaTest < ActiveSupport::TestCase
   test 'search with no matching result' do
     idea = Idea.new
     idea.title = 'Stand at the top of the Empire State Building'
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
     idea.save!
     assert_empty(Idea.search('snorkelling'))
   end
@@ -74,18 +90,23 @@ class IdeaTest < ActiveSupport::TestCase
   test 'search with two matching results' do
     idea_1 = Idea.new
     idea_1.title = 'Stand at the top of the Empire State Building'
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     idea_2 = Idea.new
     idea_2.title = 'Stand on the pyramids'
+    idea_2.user = user
     idea_2.save!
     assert_equal(2, Idea.search('Stand').length())
   end
 
   test 'search where only description matches' do
-    idea_1 = Idea.new
-    idea_1.title = 'Surfing in Portugal'
-    idea_1.description = 'See what Atlantic coast waves are like!'
-    idea_1.save!
+    idea = Idea.new
+    idea.title = 'Surfing in Portugal'
+    idea.description = 'See what Atlantic coast waves are like!'
+    user = User.new email: 'jiw@netplus.ch'
+    idea.user = user
+    idea.save!
     assert_equal 1, Idea.search('coast').length
   end
 
@@ -93,10 +114,13 @@ class IdeaTest < ActiveSupport::TestCase
     idea_1 = Idea.new
     idea_1.title = 'Overnight hike in Switzerland'
     idea_1.description = 'Stay in a Swiss refuge in the mountains.'
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     idea_2 = Idea.new
     idea_2.title = 'Hike the mountains in Italy'
     idea_2.description = 'See the Dolomites and Italian Alps.'
+    idea_2.user = user
     idea_2.save!
     assert_equal 2, Idea.search('mountains').length
   end
@@ -108,9 +132,12 @@ class IdeaTest < ActiveSupport::TestCase
   test 'most_recent method when there are two records' do
     idea_1 = Idea.new
     idea_1.title = "Idea 1"
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     idea_2 = Idea.new
     idea_2.title = "Idea 2"
+    idea_2.user = user
     idea_2.save!
     recent_ideas = Idea.most_recent
     assert_equal 2, recent_ideas.length
@@ -118,9 +145,11 @@ class IdeaTest < ActiveSupport::TestCase
   end
 
   test 'most_recent method when there are six records' do
+    user = User.new email: 'jiw@netplus.ch'
     6.times do |i|
       idea = Idea.new
       idea.title = "Idea #{i+1}"
+      idea.user = user
       idea.save!
     end
 
@@ -145,6 +174,8 @@ class IdeaTest < ActiveSupport::TestCase
 
   test 'Comments are ordered correctly' do
     idea_1 = Idea.new title: 'some title'
+    user = User.new email: 'jiw@netplus.ch'
+    idea_1.user = user
     idea_1.save!
     comment_1 = Comment.new body: 'This would be great fun', user: User.new
     comment_2 = Comment.new body: "I agree! I'd like to do this as well", user: User.new
