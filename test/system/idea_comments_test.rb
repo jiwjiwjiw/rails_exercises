ac08669d71b37d6f4eab2ad5d72a23f0ddec7e4a
Jean Iwanowski <jean.iwanowski@icloud.com>
2018-09-04 11:12:32 +0200

    fixed tests failing due to saving a comment without user

--- test/system/idea_comments_test.rb
+++ test/system/idea_comments_test.rb
@@ -2,11 +2,18 @@ require "application_system_test_case"
 
 class IdeaCommentsTest < ApplicationSystemTestCase
   test 'adding a Comment to an Idea' do
+    user = User.new email: 'jiw@netplus.ch'
+    user.save!
+    visit new_user_path
+    fill_in 'Email address', with: user.email
+    click_on 'Log in', match: :first
+
     idea = Idea.new title: 'Test idea'
     idea.save!
     visit idea_path(idea)
     fill_in 'Add a comment', with: 'Some dummy comment'
     click_on 'Post', match: :first
+    
     assert_equal idea_path(idea), page.current_path
     assert page.has_content? 'Some dummy comment'
   end
